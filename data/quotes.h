3e6aa64730d3fd5fc6f26e0f13eeb860d5b195a1
paczor@eclipsarch <paczor@fubar.pl>
2026-04-12 21:48:34 +0200

    feat: built-in quote list (verify against original noseguy.c)

---
+++ data/quotes.h
@@ -0,0 +1,20 @@
+#pragma once
+
+/* Built-in messages from the original noseguy (Dan Heller / Jamie Zawinski).
+ * TODO: replace with verbatim list from xscreensaver/hacks/noseguy.c
+ * Array is NULL-terminated. */
+static const char *builtin_quotes[] = {
+    "I nose a good time when I see one.",
+    "The nose knows.",
+    "A nose by any other name would smell as sweet.",
+    "Keep your nose clean.",
+    "Nosey? Me? Never.",
+    "Following my nose since 1992.",
+    "Plain as the nose on your face.",
+    "Something smells like a good idea.",
+    "Sniff sniff.",
+    "Nosing around, as usual.",
+    "Never look a gift nose in the mouth.",
+    "It's all relative to the size of your nose.",
+    NULL
+};
